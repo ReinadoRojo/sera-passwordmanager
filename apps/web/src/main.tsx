086cb1f02492c5fc9b284e509d681e5e13e6e4f3
Ezequiel C. <e.cf@leiuq.fun>
2025-12-31 16:37:38 +0100

    react-router

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: null,
+  },
+  {
+    path: "/vault",
+    Component: null,
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}/>,
 )
