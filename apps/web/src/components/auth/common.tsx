63af0a9b4f0be23193feb04ed240329ed1b2ee5d
Ezequiel C. <e.cf@leiuq.fun>
2026-01-01 18:54:50 +0100

    added auth header as component aside from the forms

---
+++ apps/web/src/components/auth/common.tsx
@@ -0,0 +1,9 @@
+export const AuthHeader = () => (
+    <>
+        <div className="flex flex-row space-x-4 items-center justify-center">
+            <img src="/logo.svg" alt="Sera password manager" className="size-14"/>
+            <h1 className="text-2xl font-bold text-[#d3d3d3]">Sera</h1>
+        </div>
+        <p className="mx-auto text-sm text-[#d3d3d3]">Open-source password manager</p>
+    </>
+)
